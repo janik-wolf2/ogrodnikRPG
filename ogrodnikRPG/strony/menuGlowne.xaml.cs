b0c03d101792f24da6c496a10139c9e0fdf57296
student <student@LABB-106-14>
2026-01-09 10:02:29 +0100

    Dodaj pliki projektów.

---
+++ ogrodnikRPG/strony/menuGlowne.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ogrodnikRPG.strony
+{
+    /// <summary>
+    /// Logika interakcji dla klasy menuGlowne.xaml
+    /// </summary>
+    public partial class menuGlowne : Page
+    {
+        public menuGlowne()
+        {
+            InitializeComponent();
+        }
+
+        public void showPrzeciwnicy(object sender, EventArgs e)
+        {
+            MessageBox.Show("XD");
+            NavigationService.Navigate(new Uri("strony/przeciwnicy.xaml", UriKind.Relative));
+            
+        }
+    }
+}
